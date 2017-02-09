e386dca912e8872b7800fccc05e429cd632d3c08
Jonathan Lauzon <jonathan.lauzonv4si@gmail.com>
2017-02-09 11:36:35 -0500

    Début du travail sur de nouvelles formes

--- js/script.js
+++ js/script.js
@@ -1,36 +1,49 @@
 // on cible l'élément « path » que l'on veut animer 
 var triangle = document.querySelectorAll("#monSVG path")[0];
+var cercle = document.querySelectorAll("#svg-01 circle")[0];
+var rectangle = document.querySelectorAll("#svg-02 rect")[0];
 // la longueur du chemin «path»
 var longueur = triangle.getTotalLength();
+var longueurCercle = getCircleLength(cercle);
+var longueurRectangle = getRectLength(rectangle);
 
-console.log(longueur);
+console.log(longueurRectangle);
 
 // La longueur du tracé
-// https://developer.mozilla.org/en/docs/Web/SVG/Attribute/stroke-dasharray
-
 triangle.style.strokeDasharray = longueur;
+cercle.style.strokeDasharray = longueurCercle;
+rectangle.style.strokeDasharray = longueurRectangle;
 
 // On fait disparaître le tracé en appliquant la propriété css strokeDashoffset sur toute la longueur du chemin
 triangle.style.strokeDashoffset = longueur;
+cercle.style.strokeDashoffset = longueurCercle;
+rectangle.style.strokeDashoffset = longueurRectangle;
 
 // extraire la fraction du déplacement de la barre de défilement et  ajusté le tracé
 window.addEventListener("scroll", maTrace);
 
 function maTrace() {
+	var fractionDuScroll = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
 
-	console.log('scrollTop de body = ' + document.body.scrollTop)
-	console.log('scrollTop de documentElement = ' + document.documentElement.scrollTop)
-	console.log('scrollHeight de documentElement = ' + document.documentElement.scrollHeight)	
-	console.log('clientHeight de documentElement = ' + document.documentElement.clientHeight)			
+	var trace = longueur * fractionDuScroll;
+	var traceCercle = longueurCercle * fractionDuScroll;
+	var traceRectangle = longueurRectangle * fractionDuScroll;
 
-https://developer.mozilla.org/en-US/docs/Web/API/Element/clientHeight
+	// tracé selon la fraction de «scroll»
+	triangle.style.strokeDashoffset = longueur - trace ;
+	cercle.style.strokeDashoffset = longueurCercle - traceCercle;
+	rectangle.style.strokeDashoffset = longueurRectangle - traceRectangle;
+}
 
+function getCircleLength(el) {
+	var r = el.getAttribute('r');
+	var circleLength = 2 * Math.PI * r; 
+	return circleLength;
+}
 
+function getRectLength(el) {
+	var w = el.getAttribute('width');
+	var h = el.getAttribute('height');
 
-  var fractionDuScroll = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-
-  var trace = longueur * fractionDuScroll;
-
-  // tracé selon la fraction de «scroll»
-  triangle.style.strokeDashoffset = longueur - trace ;
+	return (w*2)+(h*2);
 }
